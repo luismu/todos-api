1380d93541f60e1fac47b0ac5323d45e2e63f78a
luis murillo <lumx2@hotmail.com>
2017-07-08 17:19:46 -0500

    User authentication jwt

--- app/controllers/concerns/exception_handler.rb
+++ app/controllers/concerns/exception_handler.rb
@@ -4,12 +4,28 @@ module ExceptionHandler
   extend ActiveSupport::Concern
 
   included do
+    # Define custom handlers
+
+    # 422 - unprocessable_entity
+    rescue_from ActiveRecord::RecordInvalid, with: :four_twenty_two
+    rescue_from ExceptionHandler::AuthenticationError, with: :unauth_request
+    rescue_from ExceptionHandler::MissingToken, with: :four_twenty_two
+    rescue_from ExceptionHandler::InvalidToken, with: :four_twenty_two
+
     rescue_from ActiveRecord::RecordNotFound do |e|
       json_response({ message: e.message }, :not_found)
     end
+  end
 
-    rescue_from ActiveRecord::RecordInvalid do |e|
-      json_response({ message: e.message }, :unprocessable_entity)
-    end
+  private
+
+  # JSON response with message; Status code 422 - unprocessable entity
+  def four_twenty_two(e)
+    json_response({ message: e.message }, :unprocessable_entity)
+  end
+
+  # JSON response with message; Status code 401 - Unauthorized
+  def unauth_request(e)
+    json_response({ message: e.message }, :unauthorized)
   end
 end
